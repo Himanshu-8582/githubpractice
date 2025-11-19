664b69c8b09095541c078f7859aedac9829d6a38
Himanshu <himanshugupta8582@gmail.com>
2025-11-19 15:45:38 +0530

    sum of natural nums

---
+++ Sum.java
@@ -0,0 +1,8 @@
+public class Sum {
+    public static int sum(int n) {
+        return (n * (n - 1)) / 2;
+    }
+    public static void main(String[] args) {
+        System.out.println(sum(4));
+    }
+}
