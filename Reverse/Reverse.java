f3b032db980b05096ee385e8a31a8bb23d325e75
Himanshu <himanshugupta8582@gmail.com>
2025-12-23 21:21:12 +0530

    reverse file

---
+++ Reverse/Reverse.java
@@ -0,0 +1,13 @@
+class Reverse{
+    public static void main(String[] args) {
+        String str = "Hello World";
+        String reversedStr = "";
+
+        for(int i = str.length() - 1; i >= 0; i--){
+            reversedStr += str.charAt(i);
+        }
+
+        System.out.println("Original String: " + str);
+        System.out.println("Reversed String: " + reversedStr);
+    }
+}
